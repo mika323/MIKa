e5a6a01e1560c5033f81c7d53807e3f9f727be76
Шаститко Милана <Shastitko_MD_25@student.grsu.by>
2026-05-16 19:11:00 +0300

    Update Common.h

--- Common.h
+++ Common.h
@@ -1,1 +1,21 @@
+#pragma once
+#include <iostream>
+#include <windows.h>
 
+struct ProcessInfo {
+    DWORD pid;
+    wchar_t* name;
+    SIZE_T memoryUsage;
+    double cpuUsage;
+    double diskUsage;
+    double netUsage;
+    ProcCat category;
+    wchar_t* statusStr;
+};
+
+struct ProcessCpuTime {
+    DWORD pid;
+    ULONGLONG lastKernel;
+    ULONGLONG lastUser;
+    bool isActive;
+};
